a80d9c43c12e235a34f81e6ae4aef38e340d3196
kuikoART <cmfaponte@gmail.com>
2025-08-03 08:36:27 -0400

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'KuikoDelivery',
+    theme: ThemeData(primarySwatch: Colors.deepOrange),
+    home: HomeScreen(),
+  ));
+}
